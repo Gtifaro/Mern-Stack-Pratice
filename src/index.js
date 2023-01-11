77aa0464e06d98e2d5f31eb684774792afd01f29
Gabriel Rodríguez <gabrieltifaro2021@gmail.com>
2023-01-11 11:17:39 -0500

    Backend ready

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.set("port", process.env.PORT || 3000);
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+
 //Routes
 app.use("/api/task",require("./routes/task.routes.js"));
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const Task = require("../models/task");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const tasks = await Task.find();
     res.json({
         status: 200,
-        message: "API Working"
+        message: "API Working",
+        body: tasks
+    })
+});
+
+router.get("/:id", async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json({
+        status: 200,
+        message: "API Working",
+        body: task
+    })
+});
+
+router.post("/", async (req, res) => {
+    const {tittle, description} = req.body;
+    const newTask = new Task({tittle, description});
+    await newTask.save();
+    res.json({
+        status: 200,
+        message: "Task created"
+    });
+});
+
+router.put("/:id", async (req, res) => {
+    const {tittle, description} = req.body;
+    const newTask = {tittle, description};
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({
+        status: 200,
+        message: "Task Updated"
+    });
+});
+
+router.delete("/:id", async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({
+        status: 200,
+        message: "Task Deleted"
     });
 });
 
